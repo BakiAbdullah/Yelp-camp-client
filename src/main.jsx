6678ae7fee19ce14bd2c1aac64209b283bb2ebfd
BakiAbdullah <shatil605@gmail.com>
2023-06-07 12:54:10 +0600

    router setup authprovider setup and private route setup implemented

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import AuthProvider from "./providers/AuthProvider.jsx";
+import { Toaster } from "react-hot-toast";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes/Routes.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <AuthProvider>
+    <Toaster />
+    <RouterProvider router={router} />
+  </AuthProvider>
+);
